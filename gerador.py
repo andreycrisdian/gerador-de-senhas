9af075d65fce126eaa49f90b5d79e040e5a28075
andrey <andreycrisdiam123455@gmail.com>
2024-04-08 17:26:04 -0300

    começando

---
+++ gerador.py
@@ -0,0 +1,13 @@
+import random
+
+print("Bem vindo ao gerador de senhas!");
+
+chars = "abcdefghijklmnopqrstuvwxyz123456789ç!()*&¨%$#;:,.][[]}{]"
+
+number = input("Coloque a quantidade de senhas para ser gerada:")
+number = int(number)
+
+length = input("Coloque a quantidade de caracteres da sua senha:")
+length = int(length)
+
+print("Suas senhas:")
